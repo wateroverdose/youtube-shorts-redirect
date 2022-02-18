13534f4bee180d71256f21ad629cabb1db9d56aa
bruhfunnydev <96541955+bruhfunnydev@users.noreply.github.com>
2022-02-18 11:37:21 +0100

    another stupid ass fix

--- main.user.js
+++ main.user.js
@@ -4,18 +4,21 @@
 // @description:pl ten skrypt przenosi cię na 'watch?v=' zamiast na '/shorts/' gdy klikasz na jakiegoś shorta na youtubie
 // @author       bruhfunny (https://github.com/bruhfunnydev)
 // @version      v1
-// @match        *://*.youtube.com/shorts/*
-// @downloadURL  https://github.com/bruhfunnydev/youtube-shorts-redirect/main.user.js
-// @updateURL    https://github.com/bruhfunnydev/youtube-shorts-redirect/main.user.js
+// @match        *://*.youtube.com/*
+// @downloadURL  https://github.com/bruhfunnydev/youtube-shorts-redirect/raw/main/main.user.js
+// @updateURL    https://github.com/bruhfunnydev/youtube-shorts-redirect/raw/main/main.user.js
 // @namespace    http://tampermonkey.net
 // @icon         https://www.youtube.com/s/desktop/76df868b/img/favicon.ico
 // @run-at       document-start
-// @grant        window.onurlchange
 // @license      unlicense (unlicense.org)
 // ==/UserScript==
 
 
-if (window.location.toString().indexOf('youtube.com/shorts') > -1) {
-    newUrl = window.location.href.toString().replace(/shorts/g, 'watch');
-    document.location.href = newUrl;
+function redirect() {
+    if (window.location.toString().indexOf('youtube.com/shorts') > -1) {
+        let newUrl = window.location.href.toString().replace(/shorts/g, 'watch');
+        document.location.href = newUrl;
+    }
 }
+
+window.onload = redirect();
